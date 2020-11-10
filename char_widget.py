147796dc8dcda0e9f091283518c9554eb8ffddb7
Nathan Dumont <nathan.dumont@chronos.co.uk>
2020-11-10 10:22:34 +0000

    Added basic drag-draw support

--- char_widget.py
+++ char_widget.py
@@ -25,6 +25,7 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 from gi.repository import GObject
 from gi.repository import Gdk
+from gi.repository import GdkPixbuf
 
 class CharacterWidget(Gtk.EventBox):
   def __init__(self, font):
@@ -39,7 +40,13 @@ class CharacterWidget(Gtk.EventBox):
     self._colour_fg = Gdk.Color(font.fg['r'],
                                     font.fg['g'],
                                     font.fg['b'])
-
+    d = bytes([font.fg['r']//256, font.fg['g']//256, font.fg['b']//256] * (32*32))
+    self.drag_active = GdkPixbuf.Pixbuf.new_from_data(d, GdkPixbuf.Colorspace.RGB,
+                                                      False, 8, 32, 32, 32*3)
+    d = bytes([font.bg['r']//256, font.bg['g']//256, font.bg['b']//256] * (32*32))
+    self.drag_normal = GdkPixbuf.Pixbuf.new_from_data(d, GdkPixbuf.Colorspace.RGB,
+                                                      False, 8, 32, 32, 32*3)
+    
     self.modify_bg(Gtk.StateType.NORMAL, self._colour_frame)
 
     self.pixels = []
@@ -49,7 +56,11 @@ class CharacterWidget(Gtk.EventBox):
         self.pixels[i].append(Gtk.EventBox())
         self.pixels[i][j].modify_bg(Gtk.StateType.ACTIVE, self._colour_fg)
         self.pixels[i][j].modify_bg(Gtk.StateType.NORMAL, self._colour_bg)
+        self.pixels[i][j].set_events(self.pixels[i][j].get_events() | Gdk.EventMask.POINTER_MOTION_MASK)
         self.pixels[i][j].connect("button-press-event", self.pixel_click)
+        self.pixels[i][j].connect("drag-motion", self.pixel_drag2)
+        self.pixels[i][j].drag_source_set(Gdk.ModifierType.BUTTON1_MASK, [Gtk.TargetEntry.new(f"pix", Gtk.TargetFlags.SAME_APP, i*font.cols+j)], Gdk.DragAction.PRIVATE)
+        self.pixels[i][j].drag_dest_set(Gtk.DestDefaults.ALL, [Gtk.TargetEntry.new(f"pix", Gtk.TargetFlags.SAME_APP, i*font.cols+j)], Gdk.DragAction.PRIVATE)
         self.pixels[i][j].set_size_request(28, 28)
         self.table.attach(self.pixels[i][j], j, j+1, i, i+1, 
                           xpadding=1, ypadding=1)
@@ -60,9 +71,16 @@ class CharacterWidget(Gtk.EventBox):
   def pixel_click(self, widget, *kw):
     if widget.get_state() == Gtk.StateType.NORMAL:
       widget.set_state(Gtk.StateType.ACTIVE)
+      self._drag_state = Gtk.StateType.ACTIVE
+      widget.drag_source_set_icon_pixbuf(self.drag_active)
     else:
       widget.set_state(Gtk.StateType.NORMAL)
+      self._drag_state = Gtk.StateType.NORMAL
+      widget.drag_source_set_icon_pixbuf(self.drag_normal)
     self._modified = True
+
+  def pixel_drag2(self, widget, content, x, y, time):
+    widget.set_state(self._drag_state)
 
   def get_pixels(self):
     ret = []
@@ -92,12 +110,18 @@ class CharacterWidget(Gtk.EventBox):
     for i in range(len(self.pixels)):
       for j in range(len(self.pixels[0])):
         self.pixels[i][j].modify_bg(Gtk.StateType.ACTIVE, self._colour_fg)
+    d = bytes([r//256, g//256, b//256] * (32*32))
+    self.drag_active = GdkPixbuf.Pixbuf.new_from_data(d, GdkPixbuf.Colorspace.RGB,
+                                                      False, 8, 32, 32, 32*3)
 
   def set_bg(self, r, g, b):
     self._colour_bg = Gdk.Color(r, g, b)
     for i in range(len(self.pixels)):
       for j in range(len(self.pixels[0])):
         self.pixels[i][j].modify_bg(Gtk.StateType.NORMAL, self._colour_bg)
+    d = bytes([r//256, g//256, b//256] * (32*32))
+    self.drag_normal = GdkPixbuf.Pixbuf.new_from_data(d, GdkPixbuf.Colorspace.RGB,
+                                                      False, 8, 32, 32, 32*3)
 
   @property
   def modified(self):
